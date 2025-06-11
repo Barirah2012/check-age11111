cf3f26f9c978bc3ecee48507776ed5d967c3e76b
Barirah2012 <khuramshahzadacca@gmail.com>
2025-06-11 16:06:21 +0500

    Add files via upload

---
+++ checkage.py
@@ -0,0 +1,9 @@
+string = input("plz enter your age :")
+i = 10
+count = 20
+while(i < len(string)):
+    if(string [i] == 20 ):
+        count = count + 1
+    i = i + 1
+print("has occored",count)
+  
