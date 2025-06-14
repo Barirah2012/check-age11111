27828d590ce09484e44e8bfd82af69c0cda02368
Barirah2012 <khuramshahzadacca@gmail.com>
2025-06-14 14:37:42 +0500

    Update checkage.py

--- checkage.py
+++ checkage.py
@@ -2,8 +2,10 @@ string = input("plz enter your age :")
 i = 10
 count = 20
 while(i < len(string)):
+    #if age is more than 20 or less than 10 print sorry your not allowed
     if(string [i] == 20 ):
         count = count + 1
     i = i + 1
 print("has occored",count)
+print("sorry you are not allowed your age is not valid")
   
